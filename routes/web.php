ab2a240e6dc8ff36b4dc91eda378da64e7bd9275
Tiago Augusto Dotta - FATEC - Cuiaba/MT <tiago.dotta@alunosenai.mt>
2025-10-31 20:43:10 -0400

    second commit

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Home;
 use App\Livewire\Alunos;
+use App\Livewire\Sala;
 
 // Route::view('/', 'welcome');
 
 Route::get('/', Home::class)->name('home');
 
 Route::get('alunos', Alunos::class)->name('alunos');
+
+Route::get('sala', Sala::class)->name('sala');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
